787ce5f1af293ff374251c58b3b188ce6fd333de
Manoel Carvalho <manoelmecat@gmail.com>
2023-08-23 08:44:51 -0300

    feat: finish

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import { useRoutes } from './routes';
+
+const PORT = process.env.PORT || 8091;
+
+const app = express();
+app.use(express.json());
+useRoutes(app);
+
+
+app.listen(PORT, () => console.log('Servidor iniciado na porta ' + PORT));
