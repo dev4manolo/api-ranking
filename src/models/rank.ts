162390bc1bcc3f704b6b27d2d4b597de8d88f16b
Manoel Carvalho <manoelmecat@gmail.com>
2023-08-24 17:14:50 -0300

    fix: adjusts list rank

--- src/models/rank.ts
+++ src/models/rank.ts
@@ -3,13 +3,13 @@ import { prisma } from "../prisma/client";
 export type Rank = {
   id: string;
   name: string;
-  score: string;
+  score: number;
 };
 
 export type requestRank = {
   student_id: number;
   class_id: number;
-  score: string;
+  score: number;
 };
 
 const insertRank = async (rank: requestRank) => {
@@ -26,15 +26,16 @@ const insertRank = async (rank: requestRank) => {
 
 const listRank = async () => {
   const data: any = await prisma.$queryRaw`
-    SELECT
-        student.id,
-        student.name,
-        SUM(ranking.score) AS score
-    FROM ranking
-    JOIN student ON ranking.student_id = student.id
-    GROUP BY student.id
-    ORDER BY score DESC, student.name ASC
+    select 
+      s.id,
+      s."name",
+      sum(r.score) as score
+    FROM "Ranking" r
+    JOIN "Student" s on r.student_id = s.id
+    group by s.id 
+    order by score desc 
 `;
+
   const d = data.map((d: any) => {
     return {
       id: d.id,
