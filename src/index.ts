eba884120bc941deca545a1c3eaa594e27627efb
Manoel Carvalho <manoelmecat@gmail.com>
2023-08-25 19:21:51 -0300

    feat: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { useRoutes } from "./routes";
+import swaggerDocument from "./swagger.json";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3333;
@@ -9,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 useRoutes(app);
 
